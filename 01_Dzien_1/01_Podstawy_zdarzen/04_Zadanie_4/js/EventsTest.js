fed2aac39d168b644d4066eabbbb44d7f015d0a9
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-08-07 10:46:52 +0200

    day1 task 01_Podstawy_zdarzen task4

--- 01_Dzien_1/01_Podstawy_zdarzen/04_Zadanie_4/js/EventsTest.js
+++ 01_Dzien_1/01_Podstawy_zdarzen/04_Zadanie_4/js/EventsTest.js
@@ -8,16 +8,23 @@ const EventsTest = () => {
 	};
 	const mouseOver = () => {
 		console.log("mouseOver");
-		// alert("mouseOver");
+
 	}
 	const mouseLeave = () => {
 		console.log("mouse LEAVe");
-		// alert("mouse LEAVe");
+
+	}
+	const divStyle = {
+		width: "100px",
+		height: "100px",
+		backgroundColor: "red",
 	}
 
-
 	return (<>
-		<div onClick={click} onMouseOver={mouseOver} onMouseLeave={mouseLeave} style={{ width: "100px", height: "100px", backgroundColor: "red" }} ></div>
+		<div onClick={click}
+			onMouseOver={mouseOver}
+			onMouseLeave={mouseLeave}
+			style={divStyle} ></div>
 	</>)
 }
 export default EventsTest;
